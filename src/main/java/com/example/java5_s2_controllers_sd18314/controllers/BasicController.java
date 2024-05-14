a6613799884c334b9235fa96435391b450686ada
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-05-14 17:20:44 +0700

    Basic Controller part 2: @RequestParam

--- src/main/java/com/example/java5_s2_controllers_sd18314/controllers/BasicController.java
+++ src/main/java/com/example/java5_s2_controllers_sd18314/controllers/BasicController.java
@@ -2,6 +2,7 @@ package com.example.java5_s2_controllers_sd18314.controllers;
 
 import com.example.java5_s2_controllers_sd18314.entities.Student;
 import com.example.java5_s2_controllers_sd18314.services.StudentService;
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class BasicController {
@@ -31,9 +34,58 @@ public class BasicController {
         return "home";
     }
 
-    @GetMapping("/hello")
+    //1. @RequestParam
+    // http://localhost:8080/hello?username=Poly&email=Gmail&id=100
+    //@GetMapping("/hello")
+    //@ResponseBody
+    //public String sayHello(
+    //        @RequestParam(name = "username") String name,
+    //        @RequestParam(name = "email") @Nullable String email,
+    //        @RequestParam(name = "id", required = false) String id
+    //){
+    //    return "Hello " + name + " ! " + email + " ! " + id;
+    //}
+
+    // Optional<T>
+    // http://localhost:8080/hello?username=Poly&email=Gmail&id=100
+    //@GetMapping("/hello")
+    //@ResponseBody
+    //public String sayHelloV2c(
+    //        @RequestParam(name = "username") Optional<String> name,
+    //        @RequestParam(name = "email", required = false) String email
+    //){
+    //    return "Hello " + name.orElseGet(() -> "Not Provided") + " ! " + email;
+    //}
+
+    // default value
+    // http://localhost:8080/hello?username=Poly&email=Gmail&id=100
+    //@GetMapping("/hello")
+    //@ResponseBody
+    //public String sayHelloV2d(
+    //        @RequestParam(name = "username") Optional<String> name,
+    //        @RequestParam(name = "email", defaultValue = "default-EMAIL") String email
+    //){
+    //    return "Hello " + name.orElseGet(() -> "Not Provided") + " ! " + email;
+    //}
+
+    // get all parameters
+    // http://localhost:8080/hello?username=Poly&email=Gmail&id=100
+    //@GetMapping("/hello")
+    //@ResponseBody
+    //public String sayHelloV2e(
+    //        @RequestParam Map<String, String> allParams
+    //        ){
+    //    return "Parameters: " + allParams.entrySet();
+    //}
+
+    // http://localhost:8080/hello?username=Poly&email=Gmail&id=100
+    @GetMapping(value = "/hello", params = {"username", "email"})
     @ResponseBody
-    public String sayHello(@RequestParam String name){
-        return "Hello " + name + "!";
+    public String sayHelloV2e(
+            @RequestParam Map<String, String> allParams
+            ){
+        return "Parameters: " + allParams.entrySet();
     }
+
+
 }
